700b361356abae41ca060fd19fd442cf63242c39
glaux <mirmikh@gmail.com>
2017-05-04 21:22:07 +0300

    Add lesson 03

---
+++ 03/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const inputs = document.querySelectorAll('.controls input');
+
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+});
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
