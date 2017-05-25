6cb46640eee2590d29a61f2d3fd6dcf686514eda
glaux <mirmikh@gmail.com>
2017-05-25 14:10:48 +0300

    Add lesson 13

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const styleScss = new ExtractTextPlugin('[name].css');
 
 module.exports = {
   entry: {
-    app: './src/app.js', 
+    app: './src/app.js',
     lesson01: './src/01/script.js',
     lesson02: './src/02/script.js',
     lesson03: './src/03/script.js',
@@ -16,7 +16,8 @@ module.exports = {
     lesson08: './src/08/script.js',
     lesson10: './src/10/script.js',
     lesson11: './src/11/script.js',
-    lesson12: './src/12/script.js'
+    lesson12: './src/12/script.js',
+    lesson13: './src/13/script.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,11 +25,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.scss$/, 
+      { test: /\.scss$/,
         use: styleScss.extract({
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader'],
-          publicPath: '/dist' 
+          publicPath: '/dist'
         })
       },
       {
@@ -55,77 +56,84 @@ module.exports = {
       hash: true,
       chunks: ['lesson01'],
       filename: 'lesson01.html',
-      template: './src/01/index.html' 
+      template: './src/01/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 02',
       hash: true,
       chunks: ['lesson02'],
       filename: 'lesson02.html',
-      template: './src/02/index.html' 
+      template: './src/02/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 03',
       hash: true,
       chunks: ['lesson03'],
       filename: 'lesson03.html',
-      template: './src/03/index.html' 
+      template: './src/03/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 04',
       hash: true,
       chunks: ['lesson04'],
       filename: 'lesson04.html',
-      template: './src/04/index.html' 
+      template: './src/04/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 05',
       hash: true,
       chunks: ['lesson05'],
       filename: 'lesson05.html',
-      template: './src/05/index.html' 
+      template: './src/05/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 06',
       hash: true,
       chunks: ['lesson06'],
       filename: 'lesson06.html',
-      template: './src/06/index.html' 
+      template: './src/06/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 07',
       hash: true,
       chunks: ['lesson07'],
       filename: 'lesson07.html',
-      template: './src/07/index.html' 
+      template: './src/07/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 08',
       hash: true,
       chunks: ['lesson08'],
       filename: 'lesson08.html',
-      template: './src/08/index.html' 
+      template: './src/08/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 10',
       hash: true,
       chunks: ['lesson10'],
       filename: 'lesson10.html',
-      template: './src/10/index.html' 
+      template: './src/10/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 11',
       hash: true,
       chunks: ['lesson11'],
       filename: 'lesson11.html',
-      template: './src/11/index.html' 
+      template: './src/11/index.html'
     }),
     new HtmlWebpackPlugin({
       title: 'Lesson 12',
       hash: true,
       chunks: ['lesson12'],
       filename: 'lesson12.html',
-      template: './src/12/index.html' 
+      template: './src/12/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Lesson 13',
+      hash: true,
+      chunks: ['lesson13'],
+      filename: 'lesson13.html',
+      template: './src/13/index.html'
     }),
     styleScss
   ]
